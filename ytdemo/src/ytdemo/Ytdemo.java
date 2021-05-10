df2af3722f3de4bb7386805ddb4fd38aecb15562
Thami618 <thamngcobo1998@gmail.com>
2021-05-10 09:24:47 +0200

    my first netbeans commit

---
+++ ytdemo/src/ytdemo/Ytdemo.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ytdemo;
+
+/**
+ *
+ * @author Student
+ */
+public class Ytdemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        int passmark = 12;
+        boolean passed = false;
+        int[] scores = {4,6,2,8,12,35,9};
+        for (int unitScore : scores){
+            if(unitScore >= 12){
+                passed = true;
+                break;
+            }
+        }
+        System.out.println("At the "+ passed);
+    }
+    
+}    
+    
+
